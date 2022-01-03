c4c0562fcca20304ff3ddd9ec2bbfe524a84e75b
YuChingLin <u7@linyuqingde-iMac.local>
2022-01-03 15:00:53 +0800

    add data on coach state

--- src/store/modules/coach.js
+++ src/store/modules/coach.js
@@ -1,7 +1,28 @@
 const coach = {
-  namespace: true,
+  namespaced: true,
   state(){
-
+    return{
+      coaches:[
+        {
+          id: 'c1',
+          firstName: 'Maximilian',
+          lastName: 'Schwarzmüller',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30
+        },
+        {
+          id: 'c2',
+          firstName: 'Julie',
+          lastName: 'Jones',
+          areas: ['frontend', 'career'],
+          description:
+            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30
+        }
+      ]
+    }
   },
   mutations:{
 
@@ -10,7 +31,12 @@ const coach = {
 
   },
   getters:{
-    
+    coaches(state){
+      return state.coaches
+    },
+    hasCoach(state){
+      return state.coaches && state.coaches.length>0
+    }
   }
 
 }
