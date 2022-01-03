99c7e651cef4286dddde887f0915ed72e74e24b6
YuChingLin <u7@linyuqingde-iMac.local>
2022-01-03 13:34:33 +0800

    add App on main js file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
-import {router} from './router.js'
-const app = createApp({})
+import router  from './router'
+import store from './store'
+import App from './App.vue'
+
+const app = createApp(App)
+
 app.use(router)
+app.use(store)
+
 app.mount('#app');
