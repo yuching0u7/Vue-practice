26bd6460e86c7fce4c4db57d70207296c9e9e559
YuChingLin <u7@linyuqingde-iMac.local>
2022-01-03 16:10:23 +0800

    modified Detail page using base ui style and change route prop to true

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,4 +1,48 @@
 <template>
-  <section></section>
-  <router-link to="/contact">Contact</router-link>
+<section>
+  <base-card>
+    <h2> {{ fullName }} </h2>
+    <h3> $ {{ rate }} / hour </h3>
+  </base-card>
+</section>
+<section>
+  <base-card>
+    <h2>Interested? Reach Out Now!</h2>
+    <base-button link :to="`${$route.path}/contact`">Contact</base-button>
+  </base-card>
+</section>
+<section>
+  <base-card>
+    <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+    <p>{{ description }}</p>
+  </base-card>
+</section>
 </template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      coach: null
+    }
+  },
+  computed:{
+    fullName(){
+      return this.coach.firstName + ' ' + this.coach.lastName
+    },
+    areas(){
+      return this.coach.areas
+    },
+    description(){
+      return this.coach.description
+    },
+    rate(){
+      return this.coach.hourlyRate
+    }
+  },
+  created(){
+    this.coach = this.$store.getters['coach/coaches'].find(coach => coach.id == this.id)
+  }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ const router = createRouter({
   routes:[
     {path: '/', redirect:'/coaches'},
     {path: '/coaches', component: CoachesList,},
-    {path: '/coaches/:id', component: CoachDetail, children:[
+    {path: '/coaches/:id', component: CoachDetail, props:true, children:[
       {path:'contact', component:ContactCoadh}
     ]},
     {path: '/requests', component: RequestReceived},
