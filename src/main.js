f8e078a1093dd460dfb35ac963900ee94e78a4d9
YuChingLin <u7@linyuqingde-iMac.local>
2022-01-03 15:48:24 +0800

    add base ui card, button, badge

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import router  from './router'
 import store from './store'
 import App from './App.vue'
 
+import BaseCard from './components/base/BaseCard.vue'
+import BaseButton from './components/base/BaseButton.vue'
+import BaseBadge from './components/base/BaseBadge.vue'
+
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+
+app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
+app.component('base-badge', BaseBadge)
 
 app.mount('#app');
