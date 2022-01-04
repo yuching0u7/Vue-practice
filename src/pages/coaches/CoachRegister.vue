9ff5eb6ff7798f17b9140905868f02a8885b8278
Yu Ching Lin <ching199407@gmail.com>
2022-01-04 15:26:36 +0800

    import form component on register page

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,3 +1,20 @@
 <template>
-  Register
+<section>
+  <base-card>
+    <h2>Register as a coach!</h2>
+    <coach-form></coach-form>
+  </base-card>
+</section>
 </template>
+
+<script>
+import CoachForm from '../../components/coach/CoachForm.vue'
+export default {
+  components: {
+    CoachForm
+  }
+};
+</script>
+
+<style scoped>
+</style>
