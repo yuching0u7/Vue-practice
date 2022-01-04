592f43253a2fac5cf7298db689ee25340fbf55ba
Yu Ching Lin <ching199407@gmail.com>
2022-01-04 15:47:10 +0800

    add function to receive form data

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -2,7 +2,7 @@
 <section>
   <base-card>
     <h2>Register as a coach!</h2>
-    <coach-form></coach-form>
+    <coach-form @save-coach="addCoach"></coach-form>
   </base-card>
 </section>
 </template>
@@ -12,6 +12,12 @@ import CoachForm from '../../components/coach/CoachForm.vue'
 export default {
   components: {
     CoachForm
+  },
+  methods: {
+    addCoach(newCoach) {
+      this.$store.dispatch('coach/updateCoachList', newCoach)
+      this.$router.replace('/coaches') // redirect to coaches page
+    }
   }
 };
 </script>
